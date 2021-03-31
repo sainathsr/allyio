267d2994af82ef75383b5ca294e4110d63aa6d69
sainathsr <sainathsr95@gmail.com>
2021-03-31 13:15:37 +0530

    Expand/collapse

--- src/components/OKRTree.vue
+++ src/components/OKRTree.vue
@@ -4,9 +4,21 @@
             <ul class="objective-list">
         
            <li v-for="objective in objectives" :key="objective.id" class="objective-item">
-                {{objective.title}}
+               <div class="objective-container">
+                   <button class="expand-btn" @click="toggleExpansion(objective)">
+                       <!-- toggle icon based on expansion state of objective -->
+                    <div :class="[objective.expanded ?'arrow-down':'arrow-up']">
+                     </div>
+                    
 
-                    <ul class="key-result-list">
+                   </button>
+                   {{objective.title}}
+
+               </div>
+
+                
+    
+                    <ul class="key-result-list" v-if="objective.expanded">
                             <li v-for="keyResult in objective.keyResults" :key="keyResult.id" class="key-result-item">
                                     {{keyResult.title}}
                                 </li>
@@ -21,6 +33,12 @@
 
 <script>
 export default {
+    methods:{
+        toggleExpansion(objective)
+        {
+            objective.expanded=!objective.expanded
+        }
+    },
   props: {
     objectives: {
       type: Array
@@ -32,11 +50,11 @@ export default {
 
 <style lang="scss" scoped>
 
-// .objective-container{
-//     display: flex;
-//     align-items: center;
+.objective-container{
+    display: flex;
+    align-items: center;
 
-// }
+}
 
 .objective-list li{
       list-style-type: decimal;
@@ -52,5 +70,23 @@ export default {
 .key-result-item{
     margin-left: 25px;
     padding:10px 0px  10px 2px;
+}
+
+.arrow-up {
+  width: 0; 
+  height: 0; 
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  
+  border-bottom: 5px solid black;
+}
+
+.arrow-down {
+  width: 0; 
+  height: 0; 
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  
+  border-top: 5px solid black;
 }
 </style>
